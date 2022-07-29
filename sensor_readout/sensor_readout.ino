e3d53b6055ce43bb014bb0e379ac31992879f8cc
wngfra <wngfra@gmail.com>
2022-07-29 11:40:03 +0200

    updated sensor_readout for 5x5 array

--- sensor_readout/sensor_readout.ino
+++ sensor_readout/sensor_readout.ino
@@ -1,88 +1,95 @@
-int pin1 = A1;
-int pin2 = A2;
-int pin3 = A3;
+const int M = 5;
+int analog_pins[M] = { A0, A1, A2, A3, A4 };
 
-const int N = 9; //number of taxiles
+const int N = M*M;  //number of taxiles
 float values[N];
+float v[N];
+float v_prev[N];
+float u[N];
+float u_prev[N];
 
 unsigned long t;
 float sampling_rate = 1000;
-float samp_period = (1/sampling_rate)*1E6; //samp_period in microseconds
+float samp_period = (1 / sampling_rate) * 1E6;  //samp_period in microseconds
 
-// izhikevich parameters
-const float a = 0.02;
-const float b = 0.25;
-const float c = -55;
-const float d = 0.05;
-const float k = 1;
-const float max_potential = 30.0;
+// Izhikevich parameters
+const float A = 0.02;
+const float B = 0.25;
+const float V_theta = -55;
+const float V_res = -65;
+const float D = 0.05;
+const float K = 1;
+const float MAX_POTENTIAL = 30.0;
 
-// starting voltages
-float v[9] = { -65, -65, -65, -65, -65, -65, -65, -65, -65};
-float v_prev[9] = { -65, -65, -65, -65, -65, -65, -65, -65, -65};
-float u[9] = {b * -65, b * -65, b * -65 , b * -65, b * -65, b * -65, b * -65, b * -65, b * -65};
-float u_prev[9] = {b * -65, b * -65, b * -65, b * -65, b * -65, b * -65, b * -65, b * -65, b * -65};
 // input current
 float I = 0.0;
 
 void setup() {
   // setup digital write pins
+  pinMode(0, OUTPUT);
   pinMode(1, OUTPUT);
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+
+  // starting voltages
+  for (int i = 0; i < N; ++i) {
+    v[i] = V_res;
+    v_prev[i] = V_res;
+    u[i] = B * V_res;
+    u_prev[i] = B * V_res;
+  }
 }
 
 void analog2spike() {
   // izhikevich model
-  for (int i = 0; i < 9; i++) {
+  for (int i = 0; i < N; ++i) {
     I = values[i];
-    v[i] = v_prev[i] + (0.04 * pow(v_prev[i], 2) + 5.0 * v_prev[i] + 140.0 - u_prev[i] + k * I);
-    u[i] = u_prev[i] + (a * (b * v[i] - u_prev[i]));
+    v[i] = v_prev[i] + (0.04 * pow(v_prev[i], 2) + 5.0 * v_prev[i] + 140.0 - u_prev[i] + K * I);
+    u[i] = u_prev[i] + (A * (B * v[i] - u_prev[i]));
 
-    if (v[i] > max_potential) {
-      v[i] = c;
-      u[i] = u[i] + d;
-      // maybe set v prev to the max_potential ?
+    if (v[i] > MAX_POTENTIAL) {
+      v[i] = V_theta;
+      u[i] = u[i] + D;
+      // maybe set v prev to the MAX_POTENTIAL ?
     }
     v_prev[i] = v[i];
     u_prev[i] = u[i];
   }
 }
 
+inline void readout(int idx, int multiplier) {
+  for (int i = 0; i < M; ++i) {
+    if (i != idx) {
+      digitalWrite(idx, LOW);
+    } else {
+      digitalWrite(idx, HIGH);
+    }
+  }
+  for (int j = 0; j < M; ++j) {
+    values[j + multiplier * M] = analogRead(analog_pins[j]);
+  }
+}
+
 void loop() {
   // obtain pressure values
-  digitalWrite(1, HIGH);  
-  digitalWrite(2, LOW);
-  digitalWrite(3, LOW);
-  values[0] = analogRead(pin1);
-  values[1] = analogRead(pin2);
-  values[2] = analogRead(pin3);
-
-  digitalWrite(1, LOW);
-  digitalWrite(2, HIGH);
-  digitalWrite(3, LOW);
-  values[3] = analogRead(pin1);
-  values[4] = analogRead(pin2);
-  values[5] = analogRead(pin3);
-
-  digitalWrite(1, LOW);
-  digitalWrite(2, LOW);
-  digitalWrite(3, HIGH);
-  values[6] = analogRead(pin1);
-  values[7] = analogRead(pin2);
-  values[8] = analogRead(pin3);
+  for (int i = 0; i < M; ++i) {
+    for (int j = 0; j < M; ++j) {
+      readout(i, j);
+    }
+  }
 
   analog2spike();
-  
+
   // raw analog values
-  for (int i = 0; i < 9; i++) {
-    Serial.print(values[i]);
+  for (int K = 0; K < N; ++K) {
+    Serial.print(values[K]);
     Serial.print(' ');
     delayMicroseconds(10);
   }
-  Serial.println();  
+  Serial.println();
 
-  while (micros()-t < samp_period){
-    // wait until its time to make sure each loop happens in a sampling period
+  while (micros() - t < samp_period) {
+    // wait until its time to make sure each loop happens in A sampling period
   }
 }
