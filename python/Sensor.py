d04a1e1d5f03add99c7106e3900d0e71f66a6930
wngfra <wngfra@gmail.com>
2022-08-04 17:21:55 +0200

    added simulation mode to sensor interface

--- python/Sensor.py
+++ python/Sensor.py
@@ -8,17 +8,18 @@ from matplotlib.animation import FuncAnimation
 
 
 class Sensor:
-    def __init__(self, shape, port='/dev/ttyACM0', baudrate=115200, animated=False):
+    def __init__(self, shape, port=None, baudrate=115200, animated=False):
         try:
             if port is None:
-                raise serial.SerialException('Serial port is not specified.\n')
+                raise serial.SerialException("No serial port specified.")
             self.__ser = serial.Serial(port=port, baudrate=baudrate, timeout=1)
-            self.__simulated = False
+            self.__mode = 'serial'
         except serial.SerialException as e:
             print(e)
             print('Sensor interface operated in simulation mode.\n')
             
-            self.__simulated = True
+            self.__mode = 'simulation'
+            self.init_simulation()
             
         self.__shape = shape
         self.__values = -1*np.ones(shape, dtype=int)
@@ -36,23 +37,28 @@ class Sensor:
 
         self.__activated = False
         self.__update_thread.join()
-        self.__ser.close()
+        
+        if self.__mode == 'serial':
+            self.__ser.close()
 
     def __update_values(self):
         while self.__activated:
-            if not self.__ser.is_open:
-                self.__ser.open()
+            if self.__mode == 'serial':
+                if not self.__ser.is_open:
+                    self.__ser.open()
 
-            # Convert bytes to numpy int array
-            stream = self.__ser.readline().decode('utf-8')
-            data = [int(d) for d in stream.split(' ')[:-1]]
-            values = np.asarray(data, dtype=int)
+                # Convert bytes to numpy int array
+                stream = self.__ser.readline().decode('utf-8')
+                data = [int(d) for d in stream.split(' ')[:-1]]
+                values = np.asarray(data, dtype=int)
+            elif self.__mode == 'simulation':
+                values = np.random.randint(0, 1024, self.__shape)
 
             try:
                 self.__values = values.reshape(self.__shape)
-                print(self.__values)
             except ValueError as e:
                 print(e)
+                
 
     def __animation(self, i):
         self.__artists[0].set_data(self.values)
@@ -76,12 +82,12 @@ class Sensor:
         
         plt.show(block=False)
 
+    def init_simulation(self):
+        pass
+
     @property
     def mode(self):
-        if self.__simulated:
-            return 'simulation'
-        else:
-            return 'sensing'
+        return self.__mode
 
     @property
     def values(self):
@@ -92,24 +98,29 @@ def print_values(sensor, duration, delay=0.1):
     import time
     t0 = time.time()
     while time.time() - t0 < duration:
-        # print(sensor.values)
+        print(sensor.values)
         time.sleep(delay)
 
 if __name__ == '__main__':
+    import sys
     import glob
+    
+    if len(sys.argv) > 2:
+        ports = glob.glob('/dev/ttyACM[0-9]*')
+        p = None
 
-    ports = glob.glob('/dev/ttyACM[0-9]*')
-    p = None
-
-    for port in ports:
-        try:
-            s = serial.Serial(port)
-            s.close()
-            p = port
-            break
-        except:
-            pass
+        for port in ports:
+            try:
+                s = serial.Serial(port)
+                s.close()
+                p = port
+                break
+            except:
+                pass
+    else:
+        p = None
 
     sensor = Sensor((5, 5), p)
+    print(sensor.mode)
     
     print_values(sensor, 60.0)
