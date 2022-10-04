3b4d97ebf683630a210789ad39c8e23dcb581f34
wngfra <wngfra@gmail.com>
2022-10-04 21:01:03 +0200

    added moving human

--- scripts/start_sim.sh
+++ scripts/start_sim.sh
@@ -3,6 +3,7 @@
 # sed -i 's/highResDisplay = -1/highResDisplay = 2/g' ${COPPELIASIM_ROOT_DIR}/system/usrset.txt
 cp /shared/scripts/CMakeLists.txt /shared/scripts/package.xml src/ros2_packages/sim_ros2_interface/
 cp /shared/scripts/interfaces.txt src/ros2_packages/sim_ros2_interface/meta/
+wget https://github.com/CoppeliaRobotics/simExtPovRay/blob/master/binaries/ubuntu20_04/libsimExtPovRay.so -P ${COPPELIASIM_ROOT_DIR}/
 source install/setup.bash
 colcon build --symlink-install --packages-select control_interfaces
 source install/setup.bash
