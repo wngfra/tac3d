9480661581418016ef381a6f42c66bddb54d7981
wngfra <wngfra@gmail.com>
2023-03-11 02:02:05 +0100

    removed unused submodule

--- python/SynapticSampling.py
+++ python/SynapticSampling.py
@@ -4,52 +4,47 @@
 import nengo
 import numpy as np
 from nengo.builder.builder import Builder
+from nengo.builder.learning_rules import get_pre_ens, get_post_ens, build_or_passthrough
 from nengo.builder.operator import Operator
-from nengo.learning_rules import build_or_passthrough, get_post_ens
 from nengo.params import Default, NumberParam
 from nengo.synapses import Lowpass, SynapseParam
 
 
-class MSTDP(nengo.learning_rules.LearningRuleType):
+class SynapticSampling(nengo.learning_rules.LearningRuleType):
     """
-    Mirrored STDP learning rule.
+    Synaptic Sampling learning rule.
     """
 
     modifies = "weights"
-    probeable = ("theta", "pre_filtered", "post_filtered", "delta")
+    probeable = ("pre_filtered", "post_filtered", "delta")
 
     learning_rate = NumberParam("learning_rate", low=0, readonly=True, default=1e-6)
     pre_synapse = SynapseParam("pre_synapse", default=Lowpass(tau=0.005), readonly=True)
     post_synapse = SynapseParam("post_synapse", default=None, readonly=True)
-    theta_synapse = SynapseParam(
-        "theta_synapse", default=Lowpass(tau=1.0), readonly=True
-    )
 
     def __init__(
         self,
         learning_rate=Default,
         pre_synapse=Default,
         post_synapse=Default,
-        theta_synapse=Default,
     ):
         super().__init__(learning_rate, size_in=0)
         self.pre_synapse = pre_synapse
         self.post_synapse = (
             self.pre_synapse if post_synapse is Default else post_synapse
         )
-        self.theta_synapse = theta_synapse
 
 
-class SimMSTDP(Operator):
+class SimSS(Operator):
     def __init__(
-        self, pre_filtered, post_filtered, theta, delta, learning_rate, tag=None
+        self, pre_filtered, post_filtered, delta, learning_rate, tag=None
     ):
         super().__init__(tag=tag)
         self.learning_rate = learning_rate
 
         self.sets = []
         self.incs = []
-        self.reads = [pre_filtered, post_filtered, theta]
+        self.reads = [pre_filtered, post_filtered]
         self.updates = [delta]
 
     @property
@@ -65,51 +60,36 @@ class SimMSTDP(Operator):
         return self.reads[1]
 
     @property
-    def theta(self):
-        return self.reads[2]
-
-    @property
     def _descstr(self):
         return f"pre={self.pre_filtered}, post={self.post_filtered} -> {self.delta}"
 
     def make_step(self, signals, dt, rng):
         pre_filtered = signals[self.pre_filtered]
         post_filtered = signals[self.post_filtered]
-        theta = signals[self.theta]
         delta = signals[self.delta]
         alpha = self.learning_rate * dt
 
         def step_simmstdp():
             delta[...] = np.outer(
-                alpha * post_filtered * (post_filtered - theta), pre_filtered
+                alpha * post_filtered * (post_filtered - 0.1), pre_filtered
             )
-        # TODO implement the mstdp update rule
-        dw = np.zeros((pre.size_out, post.size_out))
-        for i in range(pre.size_out):
-            for j in range(post.size_out):
-                if i != j:
-                    t_diff = post.times - pre.times[i]
-                    dw[i, j] = self.learning_rate * (
-                        np.sum(t_diff[t_diff > 0]) - np.sum(t_diff[t_diff < 0])
-                    )
-        return dw
 
         return step_simmstdp
 
 
-@Builder.register(MSTDP)
-def build_mstdp(model, mstdp, rule):
+@Builder.register(SynapticSampling)
+def build_ss(model, ss, rule):
     """
-    Builds a `.MSTDP` object into a model.
+    Builds a `.SS` object into a model.
 
     Calls synapse build functions to filter the pre and post activities,
-    and adds a `.SimMSTDP` operator to the model to calculate the delta.
+    and adds a `.SimSS` operator to the model to calculate the delta.
 
     Parameters
     ----------
     model : Model
         The model to build into.
-    mstdp : MSTDP
+    ss : SS
         Learning rule type to build.
     rule : LearningRule
         The learning rule object corresponding to the neuron type.
@@ -122,21 +102,18 @@ def build_mstdp(model, mstdp, rule):
     conn = rule.connection
     pre_activities = model.sig[get_pre_ens(conn).neurons]["out"]
     post_activities = model.sig[get_post_ens(conn).neurons]["out"]
-    pre_filtered = build_or_passthrough(model, mstdp.pre_synapse, pre_activities)
-    post_filtered = build_or_passthrough(model, mstdp.post_synapse, post_activities)
-    theta = build_or_passthrough(model, mstdp.theta_synapse, post_activities)
+    pre_filtered = build_or_passthrough(model, ss.pre_synapse, pre_activities)
+    post_filtered = build_or_passthrough(model, ss.post_synapse, post_activities)
 
     model.add_op(
-        SimMSTDP(
+        SimSS(
             pre_filtered,
             post_filtered,
-            theta,
             model.sig[rule]["delta"],
-            learning_rate=mstdp.learning_rate,
+            learning_rate=ss.learning_rate,
         )
     )
 
     # expose these for probes
-    model.sig[rule]["theta"] = theta
     model.sig[rule]["pre_filtered"] = pre_filtered
     model.sig[rule]["post_filtered"] = post_filtered
--- python/smc.py
+++ python/smc.py
@@ -1,27 +1,26 @@
 import os
 from typing import Optional
+
 import nengo
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.stats import multivariate_normal
+from SynapticSampling import SynapticSampling
 from TouchDataset import TouchDataset
-from nengo_extras.graphviz import net_diagram
 
 _DATAPATH = os.path.join(os.path.dirname(__file__), "../data/touch.pkl")
-_IMAGE_DIM: Optional[int] = None
+_IMAGE_SIZE: Optional[int] = None
 
 # Prepare dataset
 dataset = TouchDataset(_DATAPATH, noise_scale=0.1, scope=(-1.0, 1.0))
 X_train, y_train, X_test, y_test = dataset.split_set(ratio=0.5, shuffle=True)
-_IMAGE_DIM = X_train[0].shape[0]
+_IMAGE_SIZE = X_train[0].size
 
 # Simulation parameters
-dt = 1e-3
-max_rate = 150
-amp = 1.0 / max_rate
-rate_target = max_rate  # must be in amplitude scaled units
+# dt = 1e-3
+max_rate = 200
+amp = 200.0 / max_rate
+rate_target = max_rate * amp  # must be in amplitude scaled units
 
-n_hidden = 16
+n_hidden = 36
 n_output = 36
 presentation_time = 0.2
 sigma = 10
@@ -33,20 +32,22 @@ default_intercepts = nengo.dists.Choice([0, 0.1])
 layer_confs = [
     dict(
         name="input_layer",
-        n_neurons=_IMAGE_DIM * _IMAGE_DIM,
+        n_neurons=_IMAGE_SIZE,
         max_rates=nengo.dists.Choice([rate_target]),
-        neuron=nengo.PoissonSpiking(nengo.LIFRate()),
+        neuron=nengo.PoissonSpiking(nengo.LIFRate(amplitude=amp)),
         on_chip=False,
     ),
     dict(
         name="hidden_layer",
         n_neurons=n_hidden,
+        recurrent=True,
+        learning_rule=SynapticSampling(),
+        recurrent_learning_rule=SynapticSampling()
     ),
     dict(
         name="output_layer",
-        n_neurons=_IMAGE_DIM * _IMAGE_DIM,
+        n_neurons=10,
     ),
-    dict(name="theta", n_neurons=10, neuron=nengo.SpikingRectifiedLinear()),
 ]
 
 
@@ -68,7 +69,7 @@ with nengo.Network(label="smc") as model:
     connections = []
     transforms = []
     layer_probes = []
-    shape_in = nengo.transforms.ChannelShape((_IMAGE_DIM * _IMAGE_DIM,))
+    shape_in = nengo.transforms.ChannelShape((_IMAGE_SIZE,))
     x = stim
 
     # Create layers
@@ -76,10 +77,13 @@ with nengo.Network(label="smc") as model:
         layer_conf = dict(layer_conf)  # copy, so we don't modify the original
         name = layer_conf.pop("name")
         intercepts = layer_conf.pop("intercepts", default_intercepts)
-        max_rates = layer_conf.pop("max_rates", nengo.dists.Choice([rate_target]))
+        max_rates = layer_conf.pop("max_rates", None)
         neuron_type = layer_conf.pop("neuron", default_neuron)
         on_chip = layer_conf.pop("on_chip", True)
         block = layer_conf.pop("block", None)
+        recurrent = layer_conf.pop("recurrent", False)
+        learning_rule = layer_conf.pop("learning_rule", None)
+        recurrent_learning_rule = layer_conf.pop("recurrent_learning_rule", None)
 
         # Create layer transform
         if "filters" in layer_confs:
@@ -89,13 +93,20 @@ with nengo.Network(label="smc") as model:
             # Dense layer
             n_neurons = layer_conf.pop("n_neurons")
             shape_out = nengo.transforms.ChannelShape((n_neurons,))
-            transform = nengo.Dense(
-                (shape_out.size, shape_in.size),
-                init=nengo.dists.Gaussian(0.5, 0.1),
-            )
+            if name != "input_layer":
+                transform = nengo.Dense(
+                    (shape_out.size, shape_in.size),
+                    init=nengo.dists.Gaussian(0.5, 0.1),
+                )
+            else:
+                transform = 1
+            if recurrent:
+                transform_reccurent = nengo.Dense(
+                    (shape_in.size, shape_out.size),
+                    init=nengo.dists.Gaussian(0.5, 0.1),
+                )
 
             loc = "chip" if on_chip else "host"
-            n_weights = np.prod(transform.shape)
 
         assert len(layer_conf) == 0, "Unused fields in {}: {}".format(
             [name], list(layer_conf)
@@ -118,10 +129,14 @@ with nengo.Network(label="smc") as model:
             probe = nengo.Probe(y, synapse=0.01, label="%s_p" % name)
             layer_probes.append(probe)
 
-        conn = nengo.Connection(x, y, transform=transform)
-
+        conn = nengo.Connection(x, y, transform=transform, learning_rule_type=learning_rule)
         transforms.append(transform)
         connections.append(conn)
+
+        if recurrent:
+            conn_recurrent = nengo.Connection(y, x, transform=transform_reccurent, learning_rule_type=recurrent_learning_rule)
+            transforms.append(transform_reccurent)
+            connections.append(conn_recurrent)
         x = y
         shape_in = shape_out
 
