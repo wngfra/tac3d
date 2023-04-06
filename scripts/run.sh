bc8f3d8e8b6ec991e2d18461dbcb010966877629
wngfra <wngfra@gmail.com>
2023-04-06 17:06:29 +0200

    updated simulator

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,5 +2,5 @@ source /opt/ros/humble/setup.bash
 colcon build --symlink-install
 bash -c "echo 'source /workspace/install/setup.bash' > ~/.bashrc"
 source ~/.bashrc
-pip3 install nengo nengo-extras
+pip3 install -q nengo nengo-extras
 ros2 launch active_touch mujoco.launch.py
